ed9f861c40ffdda783040cbf9f95ab0bcbd518b9
Varsha V <162611754+varshavkumar12345@users.noreply.github.com>
2024-09-16 18:27:08 +0530

    Update RegularExpressionmatching.py

--- RegularExpressionmatching.py
+++ RegularExpressionmatching.py
@@ -3,6 +3,8 @@ class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         try:
             l=re.findall(p,s)
+            for i in range(100000):
+                pass
             if l!=[] and l[0]==s:
                 return True
             return False
